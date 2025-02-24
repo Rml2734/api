7daff4c877cdee75bcf869330c22c5b9221e3000
Robml2734 <Robml2734@gmail.com>
2025-02-23 18:03:52 -0600

    Working1

--- routes/metas.js
+++ routes/metas.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+
+/* GET Lista de metas */
+router.get('/', function (req, res, next) {
   res.send('responder con metas');
 });
 
 module.exports = router;
+
+

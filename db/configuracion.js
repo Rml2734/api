a03a7cff70b5a74014be713909a58236b7b3dea5
Robml2734 <Robml2734@gmail.com>
2025-04-11 09:16:46 -0600

    Modificado configuracion.js para usar DATABASE_URL de Railway

--- db/configuracion.js
+++ db/configuracion.js
@@ -1,33 +1,19 @@
-require('dotenv').config();
+//CONFIGURACION PARA PRODUCCCION
 
-const initOptions = {};
-const pgp = require('pg-promise')(initOptions);
+const pgp = require('pg-promise')();
 
 console.log("Configurando conexión a la base de datos...");
-console.log("PGHOST:", process.env.PGHOST);
-console.log("PGPORT:", process.env.PGPORT);
-console.log("POSTGRES_USER:", process.env.POSTGRES_USER);
-console.log("POSTGRES_PASSWORD:", process.env.POSTGRES_PASSWORD);
-console.log("POSTGRES_DB:", process.env.POSTGRES_DB);
 
-const cn = {
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    ssl: {
-        rejectUnauthorized: false // Importante para Railway
-    }
-};
+const cn = process.env.DATABASE_URL; // Usar DATABASE_URL proporcionada por Railway
 
 const db = pgp(cn);
 
 module.exports = db;
 
 
-//la configuracion funciona en desarrollo
-/* require('dotenv').config();
+//CONFIGURACION PARA DESARROLLO
+
+ /*require('dotenv').config();
 
 const initOptions = {};
 const pgp = require('pg-promise')(initOptions);

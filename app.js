186dcc6023b047d4de39c9cb1b5caec930ba3cb4
Robml2734 <Robml2734@gmail.com>
2025-05-01 17:56:46 -0600

    Corregir problemas de PORT

--- app.js
+++ app.js
@@ -200,11 +200,11 @@ app.use((err, req, res, next) => {
 });
 console.log("app.js - Antes de app.listen()"); // NUEVO LOG
 // 🚀 Iniciar Servidor
-const PORT = process.env.PORT || 10000;
-console.log("app.js - Valor de PORT:", PORT);  // NUEVO LOG
-app.listen(PORT, () => {
-    console.log(`✅ Servidor escuchando en puerto ${PORT}`);
-    console.log(` Modo de entorno: ${process.env.NODE_ENV || 'development'}`); // Muestra el modo
-});
+//const PORT = process.env.PORT || 10000;
+//console.log("app.js - Valor de PORT:", PORT);  // NUEVO LOG
+//app.listen(PORT, () => {
+    //console.log(`✅ Servidor escuchando en puerto ${PORT}`);
+    //console.log(` Modo de entorno: ${process.env.NODE_ENV || 'development'}`); // Muestra el modo
+//});
 
 module.exports = app; // Exporta app (útil para tests)

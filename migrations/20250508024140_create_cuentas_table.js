2a34ced78740d0778c47025d2a0a450e11ff232e
Robml2734 <Robml2734@gmail.com>
2025-05-08 21:00:11 -0600

    Fix: Migraciones para producción

--- migrations/20250508024140_create_cuentas_table.js
+++ migrations/20250508024140_create_cuentas_table.js
@@ -8,17 +8,17 @@ exports.up = function(knex) {
         table.increments('id').primary();
         table.string('usuario').notNullable().unique();
         table.string('hash').notNullable();
-        table.string('correo').notNullable().unique();
+        table.string('correo').notNullable().unique(); // 🔥 Campo obligatorio
       })
       .createTable('metas', (table) => {
         table.increments('id').primary();
         table.text('detalles').notNullable();
-        table.date('plazo').notNullable();
-        table.string('periodo').notNullable();
-        table.integer('eventos').notNullable();
-        table.string('icono').notNullable();
-        table.integer('meta').notNullable();
-        table.integer('completado').notNullable();
+        table.date('plazo').notNullable(); // Añadido
+        table.string('periodo').notNullable(); // Añadido
+        table.integer('eventos').notNullable(); // Añadido
+        table.string('icono').notNullable(); // Añadido
+        table.integer('meta').notNullable(); // Añadido
+        table.integer('completado').notNullable(); // Añadido
         table.integer('cuenta_id').references('id').inTable('cuentas');
       });
   };
